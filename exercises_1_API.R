2d758e19670be099aa57a03ecbabcc44d3092616
suzan baert <suzan.baert@outlook.com>
2018-09-17 22:29:35 +0200

    minor changes to examples

--- exercises_1_API.R
+++ exercises_1_API.R
@@ -1,11 +1,21 @@
 # API exercises
 
 
+# --------
 #number 1
+# --------
+
+
 
 library(httr)
 
-response <- GET("http://api.open-notify.org/iss-now.json")
+
+# url
+url_ex1 <- "http://api.open-notify.org/iss-now.json"
+
+
+# GET request
+response <- GET(url_ex1)
 
 
 #json data
@@ -34,18 +44,37 @@ content_parsed <- content(response)
 library(httr)
 library(jsonlite)
 
+
+#url needed: "http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON"
+
+
+# build your URL
 lat <- 4.3517103
 lon <- 50.8503396
 
 
-#url needed: "http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON"
-
 #option 1: paste together
-url <- paste0("http://api.open-notify.org/iss-pass.json?lat=", lat, "&lon=", lon)
+url_ex2 <- paste0("http://api.open-notify.org/iss-pass.json?lat=", lat, "&lon=", lon)
 
 
 #option2: 
 url_built <- build_url("http://api.open_notify.org/iss-pass.json", list(lat = 4.35, lon=50.85))
 
-GET(url)
 
+
+# GET request
+response_ex2 <- GET(url_ex2)
+
+
+# parse data
+content_ex2 <- content(response_ex2)
+
+
+#getting your info out
+str(content_ex2, max.depth =1 )
+
+#extracting risetime
+risetime <- purrr::map(content_ex2$response, "risetime")
+risetime <- purrr::map_dbl(content_ex2$response, "risetime")
+
+risetime <- purrr::map_df(content_ex2$response, c("risetime", "duration"))
