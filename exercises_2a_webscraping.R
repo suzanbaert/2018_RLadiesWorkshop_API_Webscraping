9cf29d0690950fcf8358ff5b9782de0d4f71f05d
Suzan Baert <suzan.baert@hederainsights.com>
2018-09-27 15:28:07 +0200

    DC scraping exercise

--- exercises_2a_webscraping.R
+++ exercises_2a_webscraping.R
@@ -1,7 +1,21 @@
+library(rvest)
+library(dplyr)
+
+
+
+#---------------
+# sTEP 1 & 2
+#---------------
+
 
 url_DC <- "https://www.datacamp.com/courses/free-introduction-to-r"
 
 local_html_DC <- read_html(url_DC)
+
+
+#---------------
+# sTEP 3
+#---------------
 
 
 #selecting parts
@@ -16,8 +30,16 @@ local_html_DC %>%
 
 
 
+#---------------
+# sTEP 4
+#---------------
 
-# selecting info
+# parsing to R
+local_html_DC %>% 
+  html_nodes(css = "h4") %>% 
+  
+
+# parsing to R
 local_html_DC %>% 
   html_nodes(css = ".header-hero__stat--participants") %>% 
   html_text() %>% 
@@ -25,5 +47,81 @@ local_html_DC %>%
 
 
 
+
+#---------------
+# GETTING MORE THAN ONE
+#---------------
+
+
+#course overview
+
+course_overview_url <- "https://www.datacamp.com/courses/tech:r"
+
+local_copy <- read_html(course_overview_url)
+
+
+local_copy %>% 
+  html_nodes(css = "a")
+
+
+all_course_links <- local_copy %>% 
+  html_nodes(css = ".course-block > a") %>% 
+  html_attr(name = "href")
+
+
+
+
+# function to get participants
+
+course_link <- all_course_links[1]
+
+
+url <- httr::modify_url("https://www.datacamp.com", path = course_link)
+local_copy <- read_html(url)
+n_partic <- local_copy %>% 
+  html_nodes(css = ".header-hero__stat--participants") %>% 
+  html_text() %>% 
+  readr::parse_number()
+
+course_title <- stringr::str_remove(course_link, "/courses/")
+
+df <- data.frame(date = Sys.Date(),
+           course = course_title,
+           participants <- n_partic,
+           stringsAsFactors = FALSE)
+
+
+
+
+#☺ build a function
+
+
+get_participants <- function(course_link) {
+  
+  url <- httr::modify_url("https://www.datacamp.com", path = course_link)
+  local_copy <- read_html(url)
+  n_partic <- local_copy %>% 
+    html_nodes(css = ".header-hero__stat--participants") %>% 
+    html_text() %>% 
+    readr::parse_number()
+  
+  course_title <- stringr::str_remove(course_link, "/courses/")
+  
+  df <- data.frame(date = Sys.Date(),
+                   course = course_title,
+                   participants = n_partic,
+                   stringsAsFactors = FALSE)
+  Sys.sleep(1)
+  
+  df
+  
+}
+
+
+
+#get 5 of them
+
+get_participants(course_link)
+purrr::map_df(all_course_links[1:5], get_participants)
 
 
